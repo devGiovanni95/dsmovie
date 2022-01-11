c8fcfacb89c8c2f5b50b0d3958166824358cb155
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-01-10 23:16:22 -0300

    Created link for Github

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-
+import {ReactComponent as GithubIcon } from './assets/img/github.svg';
 
 function App() {
   return(
-     <h1> Página Principal</h1>
+    <header>
+      <nav>
+        <div>
+          <h1>DSMovie</h1>
+          <a href="https://github.com/devGiovanni95">
+            <div>
+              <GithubIcon/>
+              <p>/devGiovanni95</p>
+            </div>
+            </a>
+        </div>
+      </nav>
+    </header>
     );
 }
 
