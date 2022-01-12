8b498b178c0c175f1f15ece1db4f3f02ced3800d
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-01-11 21:52:35 -0300

    Finished created Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,17 +1,17 @@
 import {ReactComponent as GithubIcon } from 'assets/img/github.svg';
-
+import './styles.css';
 
 function Navbar(){
 
     return(
         <header>
-        <nav>
-          <div>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
             <a href="https://github.com/devGiovanni95">
-              <div>
+              <div className="dsmovie-contact-conteiner">
                 <GithubIcon/>
-                <p>/devGiovanni95</p>
+                <p className="dsmovie-contact-link">/devGiovanni95</p>
               </div>
               </a>
           </div>
