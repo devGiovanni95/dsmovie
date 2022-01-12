1ab7f7b2532ba126ea8a62941e52f3801682f5f6
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-01-11 21:02:38 -0300

    Create a React Component Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon } from '../../assets/img/github.svg';
+
+
+function Navbar(){
+
+    return(
+        <header>
+        <nav>
+          <div>
+            <h1>DSMovie</h1>
+            <a href="https://github.com/devGiovanni95">
+              <div>
+                <GithubIcon/>
+                <p>/devGiovanni95</p>
+              </div>
+              </a>
+          </div>
+        </nav>
+      </header>
+    )
+}
+
+export default Navbar;
